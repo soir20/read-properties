d61222ac599d7e46922a94dbbb35d181b7b208e1
soir20 <71418127+soir20@users.noreply.github.com>
2022-08-25 23:48:18 -0400

    Retrieve specific property

--- ReadProperties.main.kts
+++ ReadProperties.main.kts
@@ -4,12 +4,13 @@ import java.io.FileInputStream
 import java.util.Arrays
 import java.util.Properties
 
-if (args.size != 1) {
+if (args.size != 2) {
     val argsStr = Arrays.toString(args)
-    throw IllegalArgumentException("Expected 1 argument, provided: $argsStr")
+    throw IllegalArgumentException("Expected 2 arguments, provided: $argsStr")
 }
 
 val fileName = args[0]
+val property = args[1]
 if (!fileName.endsWith(".properties")) {
     throw IllegalArgumentException("File $fileName is not a properties file")
 }
@@ -18,7 +19,6 @@ FileInputStream(fileName).use { stream ->
     val properties = Properties()
     properties.load(stream)
 
-    properties.entries.forEach{ (key, value) ->
-        println("::set-output name=$key::$value")
-    }
+    val value = properties.getProperty(property)
+    println("::set-output name=value::$value")
 }
